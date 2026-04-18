5de3a2e903b137bc914401e8aba86fa844d7b1f4
Nadia M <tm53929@mail.harpercollege.edu>
2026-04-18 10:05:42 -0700

    Final Submission

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -12,16 +12,26 @@ public class AppTest {
 
     @Test public void testSingleElement() {
         ArrayList<Integer> list = new ArrayList<>();
-        assertEquals(new ArrayList<>(), App.removeDuplicates(list));
+        list.add(1);
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        assertEquals(expected, App.removeDuplicates(list));
     }
 
     @Test public void  testNoDuplicates() {
-       ArrayList<String> list = new ArrayList<>();
-       list.add("A");
-       list.add("A");
-       list.add("B");
-       
-       ArrayList<String> expected = new ArrayList<>();
+       ArrayList<Integer> list = new ArrayList<>();
+       list.add(1);
+       list.add(2);
+       list.add(3);
+       assertEquals(new ArrayList<>(list), App.removeDuplicates(list));
+    }
+
+    @Test public void testWithDuplicates() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("A");
+        list.add("B");
+        ArrayList<String> expected = new ArrayList<>();
        expected.add("A");
        expected.add("B");
        assertEquals(expected, App.removeDuplicates(list));
